50b71d6c66b0f8d933092dd78f4e8bd04ba4afe6
gylee-ver <10253gy@gmail.com>
2025-05-26 11:36:35 +0900

    feat : hotjar code import

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        {/* Google Analytics */}
+        {/* Google Analytics 4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-8R9N5SG6WM"
           strategy="afterInteractive"
@@ -87,6 +87,20 @@ export default function RootLayout({
             gtag('js', new Date());
 
             gtag('config', 'G-8R9N5SG6WM');
+          `}
+        </Script>
+
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6415192,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
         </Script>
         
