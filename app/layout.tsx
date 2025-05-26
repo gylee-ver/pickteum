29b351ef8cfe6ad2e2fbbb88269ae46bd4ada895
gylee-ver <10253gy@gmail.com>
2025-05-26 11:14:07 +0900

    feat : GA4 Tagmanager code import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -74,13 +75,35 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MJSWS58K');
+          `}
+        </Script>
+        
         {/* 네이버 서치어드바이저 메타태그 */}
         <meta name="naver-site-verification" content="YOUR_CODE" />
         
         {/* 다음 서치어드바이저 메타태그 */}
         <meta name="kakao-site-verification" content="YOUR_CODE" />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MJSWS58K"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   )
 }
