8f242bc450818f23b1d91a3b2a7b0c6290f11d83
gylee-ver <10253gy@gmail.com>
2025-06-05 17:02:52 +0900

    날짜-시간 오류 해결

--- components/header.tsx
+++ components/header.tsx
@@ -8,6 +8,8 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import supabase from "@/lib/supabase"
 import { logger } from "@/lib/utils"
+import { format } from "date-fns"
+import { ko } from "date-fns/locale"
 
 interface SearchResult {
   id: string
